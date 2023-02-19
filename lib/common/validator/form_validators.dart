0191a587b269f1fa99e0111118877560fb7a742c
reza <najafireza1996@gmail.com>
2023-02-19 16:06:53 +0330

    form validator is created

---
+++ lib/common/validator/form_validators.dart
@@ -0,0 +1,66 @@
+
+
+String? nationalCodeValidator(String? value) {
+ 
+}
+
+String? numberValidator(String? value) {
+
+}
+
+String? requiredValidator(String? value) {
+
+}
+
+String? positiveNumberValidator(String? value) {
+
+}
+
+String? nameValidator(String? value) {
+
+}
+
+String? lastNameValidator(String? value) {
+
+}
+
+String? phoneNumberValidator(String? value) {
+
+}
+
+bool? phoneNumberIsValid(String? value) {
+
+}
+
+String? telNumberValidator(String? value) {
+
+}
+
+String? passwordValidator(String? value) {
+
+}
+
+
+String? addressValidator(String? value) {
+
+}
+
+String? multiSelectorRequiredValidator(List<int>? values) {
+
+}
+
+String? singleSelectorRequiredValidator(int? value) {
+
+}
+
+String? emailValidator(String? value) {
+
+}
+
+String? requiredEmailValidator(String? value) {
+
+}
+
+String? postalCodeValidator(String? value) {
+
+}
